368d8bf4e53d153545a709cb5a7d685ea7b7f0e1
LeonRemy <leonremyy2308@gmail.com>
2026-05-31 16:32:48 +0700

    Hoan thien cay

--- BTT13/BST.c
+++ BTT13/BST.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
-typedef stuct Node{
+typedef struct {
     int data;
     struct Node* left;
     struct Node* right;
 }Node;
+
+Node* Create_Node(int data) {
+    Node* n = (Node*)malloc(sizeof(Node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+typedef struct {
+    Node* root;
+} Tree;
+
+Tree* Create_Tree() {
+    Tree* t;
+    t->root = NULL;
+}
 
